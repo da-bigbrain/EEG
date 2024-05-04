da929ee56e5bd94583cc4c407f3a01348996515b
Enkhbold Ganbold <enkhbold470@gmail.com>
2024-05-04 10:34:01 -0700

    upload this code

--- bigbrain-v1/bigbrain-v1.ino
+++ bigbrain-v1/bigbrain-v1.ino
@@ -1,14 +1,17 @@
 int soundCard = A0;
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
   Serial.begin(2000000);
   Serial.println("Starting...");
   pinMode(soundCard, INPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
   int alphaData = analogRead(soundCard);
   // Serial.print("data = ");
+
   Serial.println(alphaData);
 }
